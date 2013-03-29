7de0508001f904aa2ea6d357219d6ee89a4e6488
Alex Fogg <me@alexfogg.com>
2013-03-28 20:08:04 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gosling::Application.config.secret_token = 'd414a6447c7c4309d5eb26f53f63451b935b59058e9cd5f6232ed741e88b8df223362120316394226b27e6bd6c1eafbedb07bc5c90faaa6d00c854eac006de02'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Gosling::Application.routes.draw do
+  root :to => 'home#index'
+end
