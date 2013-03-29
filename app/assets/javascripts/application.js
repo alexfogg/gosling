6d486347c3fe892086273621a73694f5c4536bbe
Alex Fogg <me@alexfogg.com>
2013-03-29 18:04:31 -0400

    added modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,5 @@
 //= require jquery_ujs
 //= require 'foundation/js/vendor/custom.modernizr.js'
 //= require 'foundation/js/foundation.min.js'
+//= require 'reveal/jquery.reveal.js'
 //= require 'gosling.js.coffee'
