ca94652c3575b9e991ee5b33dc833e92aaf5d3d6
Adi Singh <adityasingh89@gmail.com>
2013-03-30 16:14:44 -0400

    more stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,18 +4,21 @@ class HomeController < ApplicationController
 
 
   def call
+    session[:name] = params[:name]
     client= Twilio::REST::Client.new(ENV['TW_SID'], ENV['TW_TOK'])
     @call = client.account.calls.create(
       :from => '+17329630742',
-      :to => '+17324039102',
-      :url => 'http://4w6e.localtunnel.com/voice'
+      :to => '+16095589593',
+      :url => "http://4gc6.localtunnel.com/voice"
     )
+
     redirect_to root_path
-    end
+  end
 
 
-    def voice
+  def voice
     response = Twilio::TwiML::Response.new do |r|
+      r.Say session[:name]
       r.Play 'http://demo.twilio.com/hellomonkey/monkey.mp3'
     end
     render :xml => response.text
